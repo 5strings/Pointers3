def26b13332a35b6ed7a8541f1281f4f450e3ea9
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-27 17:04:15 +1100

    main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  IncreDecrOnPointers
+//
+//  Created by Eun Jae Lee on 27/11/17.
+//  Copyright © 2017 Eun Jae Lee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    int a[5] = {0,1,2,3,4};
+
+    int *p;
+    p = &a[0];
+    
+    printf("Value at p is %p\n",p);//or %u address
+    printf("Value at the address of p is %d\n\n",*p);
+    
+    p++; // address is increase by 4 byes
+    printf("Value at p is %p\n", p);
+    printf("Value at the address of p is %d\n\n",*p);
+    
+    p++; // address is increase
+    printf("Value at p is %p\n", p);
+    printf("Value at the address of p is %d\n\n",*p);
+    
+    getchar();
+    
+}
